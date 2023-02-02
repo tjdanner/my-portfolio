0e1c1ce68c6531bc7a1c3647c87cc5cca75ac613
Trevor Danner <tjdanner01@gmail.com>
2023-02-02 12:23:44 -0600

    Commiting current state to work on projects section.

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ for (let i = 0; i < icons.length; i++) {
         icons[i].style.transition = '300ms'
         icons[i].style.transform = 'scale(1.15)'
         label[i].style.display = 'block';
-        label[i].style.animation = '500ms fadeIn'
+        label[i].style.animation = '300ms fadeIn'
         icons[i].addEventListener('mouseout', e => {
             icons[i].style.transform = '';
-            label[i].style.animation = '500ms fadeOut';
-            setTimeout(e => { label[i].style.display = ''; icons[i].style.transition = ''; }, 490)
+            label[i].style.animation = '300ms fadeOut';
+            setTimeout(e => { label[i].style.display = ''; icons[i].style.transition = ''; }, 290)
         })
     })
     /* backButton.addEventListener('mouseover', e=> {
@@ -26,21 +26,26 @@ for (let i = 0; i < icons.length; i++) {
 }
 
 icons[0].addEventListener('click', e => {
-    iconContainer.style.animation = '100ms fadeOut';
-    setTimeout(e=> {iconContainer.style.display = 'none'}, 100)
-    setTimeout(e=>{gsap.from(about, { duration: .5, x: '-150%' }); about.style.display = 'flex';}, 100)
-    setTimeout(e => { back.style.display = 'block'; }, 500);
+    iconContainer.style.animation = '500ms fadeOut';
+    setTimeout(e => { iconContainer.style.display = 'none' }, 500)
+    setTimeout(e => { gsap.from(about, { duration: 1, x: '-150%' }); about.style.display = 'flex'; }, 500)
+    setTimeout(e => { back.style.display = 'block'; }, 1000);
     back.style.animation = '500ms fadeIn';
 })
 
 back.addEventListener('click', e => {
-    gsap.to(about, { duration: .5, x: '-150%' })
+    gsap.to(about, { duration: 1, x: '-150%' })
     setTimeout(e => { about.style.display = ''; }, 1000)
     back.style.animation = '100ms fadeOut';
     back.style.display = '';
-    setTimeout(e => { iconContainer.style.display = ''; }, 500);
-    gsap.from(icons, { duration: .5, y: '1050%', delay: .5, stagger: .25 });
+    setTimeout(e => { iconContainer.style.display = ''; }, 1000);
+    gsap.from(icons, { duration: .5, y: '1050%', delay: 1, stagger: .25 });
     iconContainer.style.animation = '';
+})
+
+icons[2].addEventListener('click', e=> {
+    iconContainer.style.animation = '500ms fadeOut';
+    setTimeout(e => { iconContainer.style.display = 'none' }, 490);
 })
 
 gsap.from('h1', { duration: .5, y: '-1000%' });
