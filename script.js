283a8bb18c0f428067059417bb55ab397fdd0656
Trevor Danner <tjdanner01@gmail.com>
2023-02-02 22:51:38 -0600

    Animated project labels.

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const label = document.getElementsByClassName('label');
 const about = document.getElementById('about');
 const projectsContainer = document.getElementById('projects-container');
 const projects = document.getElementsByClassName('projects');
+const projectLabels = document.getElementsByClassName('project-label')
 
 gsap.from('h1', { duration: .5, y: '-1000%' });
 gsap.from(icons, { duration: .5, y: '2000%', delay: .5, stagger: .2 });
 
 for (let i = 0; i < icons.length; i++) {
     icons[i].addEventListener('mouseover', e => {
-        icons[i].style.transition = '300ms'
-        icons[i].style.transform = 'scale(1.15)'
+        icons[i].style.transition = '300ms';
+        icons[i].style.transform = 'scale(1.15)';
         label[i].style.display = 'block';
         label[i].style.animation = '300ms fadeIn'
         icons[i].addEventListener('mouseout', e => {
@@ -73,3 +74,12 @@ back.onclick = e => {
     setTimeout(e => { iconContainer.style.display = ''; }, 500)
     iconContainer.style.animation = '500ms fadeIn';
 };
+
+for (let i = 0; i < projectLabels.length; i++) {
+    projectLabels[i].onmouseover = e => {
+        projectLabels[i].style.transform = 'scale(1.15)';
+        projectLabels[i].onmouseout = e => {
+            projectLabels[i].style.transform = '';
+        }
+    }
+}
